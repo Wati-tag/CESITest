a12ebbad2684badc6b850a60f2e369a50b7d0d2f
LilianDubois <liliandubois1209@gmail.com>
2022-01-18 11:38:32 +0100

    firt commit4

--- obj/Debug/netcoreapp3.0/MainWindow.g.cs
+++ obj/Debug/netcoreapp3.0/MainWindow.g.cs
@@ -85,7 +85,7 @@ namespace prosit1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/prosit1;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/prosit1_kqlxan3t_wpftmp;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
